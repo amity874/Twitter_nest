abfa05e07b8d950d31513f9131da58d13e304b88
Amit <amity552000@gmail.com>
2022-02-02 16:57:30 +0530

    Controller addded

--- src/hashtags/hashtags.controller.ts
+++ src/hashtags/hashtags.controller.ts
@@ -1,8 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 @Controller('hashtags')
 export class HashtagsController {
   @Get('/')
   getHashtags(): string {
     return 'top k hashtags';
+  }
+  @Get('/:tag/posts')
+  getpostForHashtag(@Param() param): string {
+    return 'show all posts with hasjtags ${param.tag}';
   }
 }
