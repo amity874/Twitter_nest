755d134b811181c870c5d609bcbe6a151b6f9995
Amit <amity552000@gmail.com>
2022-02-02 17:52:58 +0530

    Post controller and user controller done

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  @Get('/')
+  getAllPosts(): string {
+    return 'details of the uernames';
+  }
+  @Get('/:postId')
+  getPpostDetails(@Param() param): string {
+    return `details of user id = ${param.userId}`;
+  }
+  @Post('/')
+  createNewPost(): string {
+    return 'you post was created';
+  }
+  @Delete('/:postId')
+  deletePost(@Param() param): string {
+    return `delete postid ${param.postId} `;
+  }
+  @Put('/:postId/like')
+  likePost(@Param() parent): string {
+    return `liked post $ {param.postId}`;
+  }
+  @Delete('/:postId/like')
+  unlikePost(@Param() parent): string {
+    return `Unlike post $ {param.postId}`;
+  }
+}
