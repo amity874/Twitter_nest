ebe0bfae43e987b3191cdd9567d26d5a2b9adf0b
Amit <amity552000@gmail.com>
2022-02-02 17:28:00 +0530

    Added

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,3 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post, Put } from '@nestjs/common';
+import { userInfo } from 'os';
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('/@:username')
+  getUserByUsername(@Param() param): string {
+    return `details of the uernames = ${param.username}`;
+  }
+  @Get('/:userId')
+  getUserByuserId(@Param() param): string {
+    return `details of user id = ${param.userId}`;
+  }
+  @Put('/:userId/follow')
+  followuser(): string {
+    return 'you follow userInfo';
+  }
+  @Get('/:userId/followees')
+  getFolloweesofUser(): string {
+    return `get followees of given user`;
+  }
+}
